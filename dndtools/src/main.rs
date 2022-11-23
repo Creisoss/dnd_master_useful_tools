f8ac7171162587ff7e8f22798cc39d5752141152
Creisoss <eduardolucas230700@gmail.com>
2022-11-22 22:40:40 -0300

    Rewrote the entire system of rolling dices

--- dndtools/src/main.rs
+++ dndtools/src/main.rs
@@ -10,8 +10,39 @@ struct DiceRoll {
     operation_number: u32,
 }
 
+fn main(){
+    loop{
+        clear();
+        println!("Create a new dice");
+        let x = DiceRoll::new_dice_roll();
+        roll_dice(x);
+        println!("do you want to stop(y, leave blank to repeat)");
+        let op = input_char();
+        if op == 'y' { break; }
+    }
+}
+
 impl DiceRoll{
-    fn new_dice_roll(number_of_rolls: u32, dice_size: u32, operator: char, operation_number: u32) -> DiceRoll{
+    fn new_dice_roll() -> DiceRoll{
+        
+        println!("How many times will you roll your dice? ");
+        let number_of_rolls = input_number();
+        
+        println!("Hom many sides your dice has? ");
+        let dice_size = input_number();
+        
+        println!("Will you use any operation? (+, -, *, /, leave blank if not) ");
+        let operator = input_char();
+        
+        let mut operation_number: u32 = 0;
+        if operator == '+' || operator == '-' || operator == '/' || operator == '*'{
+            println!("What number will you use in your operation? ");
+            operation_number = input_number();
+            if operator == '/' && operation_number == 0 {
+                panic!("cannot divide by zero");
+            }
+        }
+        
         let roll: DiceRoll = DiceRoll { 
             number_of_rolls, 
             dice_size, 
@@ -20,83 +51,8 @@ impl DiceRoll{
         roll
     }
 }
-
-
-fn remove_whitespace(s: &mut String) {
-    s.retain(|c| !c.is_whitespace());
-}
-
-fn process_input(s: &String){
-    // all this variables will be used to process the string but will be deleted after the scope
-    // of the function ends
-    let mut index: usize = 0;
-    let mut first_number_end_index: usize;
-    let mut number_of_rolls: u32 = 0;
-    let mut second_number_first_index: usize = 0;
-    let mut second_number_last_index: usize;
-    let mut valuable_index: usize = 0;
-    let mut read_second_value: bool = false;
-    let mut dice_size: u32 = 0;
-    let mut has_operator = false;
-    let mut operator: char = '|';
-    let mut operation_number_index = 0;
-    let mut operation_number: u32 = 0;
-    let mut last_char: char = 'a';
-    for i in s.chars(){
-        if index == 0 && !i.is_numeric(){
-            panic!("The first value need to be a number");
-        }
-            
-        if !i.is_numeric() && (i == 'd' || i == 'D'){
-            first_number_end_index = index;
-            number_of_rolls = (&s[0..first_number_end_index].parse::<u32>().unwrap()).clone();
-            second_number_first_index = index + 1;
-            read_second_value = true;
-        }else if !i.is_numeric() && !(i == 'd' || i == 'D') && read_second_value == false{ 
-            panic!("You need to input d or D to set the number of sides of the dice");
-        }
-
-        if read_second_value{
-            if !i.is_numeric(){
-                if index == second_number_first_index{
-                    panic!("The second number need to be an int");
-                }
-            }else if (index == s.len() - 1) || (!i.is_numeric()) {
-                if !i.is_numeric() && last_char.is_numeric(){
-                    second_number_last_index = index - 1;
-                }else{
-                    second_number_last_index = index;
-                }
-                println!("{}",&s[second_number_first_index..second_number_last_index+1]);
-                dice_size = (&s[second_number_first_index..second_number_last_index+1].parse::<u32>().unwrap()).clone();
-                if index == s.len() - 1 && !i.is_numeric() {
-                    match i {
-                        '+' => operator = '+',
-                        '-' => operator = '-',
-                        '/' => operator = '/',
-                        '*' => operator = '*',
-                        _ => panic!("Didn't recognize this symbol"),
-                    }; 
-                    has_operator = true;
-                }
-            }
-        }
-        
-        if has_operator && i.is_numeric() {
-            operation_number_index = index;
-        }else if has_operator && !i.is_numeric(){
-            if index == s.len() - 1{
-                panic!("The third value need to be a number");
-            }else{
-                operation_number = (&s[operation_number_index..index - 1].parse::<u32>().unwrap()).clone();
-            }
-        }
-        last_char = i;
-        index = index + 1;
-    }
-    println!("{number_of_rolls}, {dice_size}, {operator}, {operation_number}");
-    let roll = DiceRoll::new_dice_roll(number_of_rolls, dice_size, operator, operation_number);
-    roll_dice(roll);
+fn clear(){
+    print!("\x1B[2J\x1B[1;1H");
 }
 
 fn roll_dice(roll: DiceRoll){
@@ -105,25 +61,47 @@ fn roll_dice(roll: DiceRoll){
     print!("{{");
     let mut x;
     for i in 0..roll.number_of_rolls{
+        
         if (i != roll.number_of_rolls) && (i != 0) {
             print!(", ");
         }
+        
         if roll.dice_size == 1 {
             x = 1;
-            total = total + x;
         }else {
-            x = rng.gen_range(1, roll.dice_size);
-            total = total + x;
+            x = rng.gen_range(1, roll.dice_size+1);
         }
+        match roll.operator {
+            '+' => x + roll.operation_number,
+            '-' => x - roll.operation_number,
+            '*' => x * roll.operation_number,
+            '/' => x / roll.operation_number,
+             _ => 1+1,
+        };
+
+        total = total + x;
         print!("{x}");
     }
     println!("}}");
     println!("{}", total);
 }
 
-fn main() {
-    let mut dice_roll: String = String::new(); 
-    io::stdin().read_line(&mut dice_roll).expect("Failed to read line");
-    remove_whitespace(&mut dice_roll);
-    process_input(&dice_roll);
+fn input_char() -> char{
+    let mut input_text = String::new();
+    io::stdin().read_line(&mut input_text).expect("failed to read from stdin");
+    let trimmed = input_text.trim();
+    match trimmed.parse::<char>() {
+        Ok(i) =>   i,
+        Err(..) => '\0',
+    }
+}
+
+fn input_number() -> u32{
+    let mut input_text = String::new();
+    io::stdin().read_line(&mut input_text).expect("failed to read from stdin");
+    let trimmed = input_text.trim();
+    match trimmed.parse::<u32>() {
+        Ok(i) =>   i,
+        Err(..) => 0,
+    }
 }
